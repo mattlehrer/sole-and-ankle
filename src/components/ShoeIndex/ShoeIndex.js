e5aa19d6a92d9eb70f586293dd5e234300b116d9
Matt Lehrer <lehrerm@gmail.com>
2021-04-04 21:37:25 -0700

    exercise 3

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,58 +1,66 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-
 import { WEIGHTS } from '../../constants';
-
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
+import ShoeSidebar from '../ShoeSidebar';
+import Spacer from '../Spacer';
 
 const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={34} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<LeftColumn>
+				<Breadcrumbs>
+					<Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+					<Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+				</Breadcrumbs>
+				<Spacer size={42} />
+				<ShoeSidebar />
+			</LeftColumn>
+			<MainColumn>
+				<Header>
+					<Title>Running</Title>
+					<Select
+						label="Sort"
+						value={sortId}
+						onChange={(ev) => setSortId(ev.target.value)}
+					>
+						<option value="newest">Newest Releases</option>
+						<option value="price">Price</option>
+					</Select>
+				</Header>
+				<Spacer size={34} />
+				<ShoeGrid />
+			</MainColumn>
+		</Wrapper>
+	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	gap: 32px;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+	width: 248px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+	flex: 1;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+	display: flex;
+	align-items: flex-start;
+`;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.5rem;
+	font-weight: ${WEIGHTS.medium};
+	margin-right: auto;
+	line-height: 1em;
 `;
 
 export default ShoeIndex;
