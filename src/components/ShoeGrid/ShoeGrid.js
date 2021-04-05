82da7a90e6a7fa3fcfb7af30f996a89c157dbdbf
Matt Lehrer <lehrerm@gmail.com>
2021-04-04 22:16:07 -0700

    exercise 4A

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-
 import SHOES from '../../data';
 import ShoeCard from '../ShoeCard';
 
 const ShoeGrid = () => {
-  return (
-    <Wrapper>
-      {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
-      ))}
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			{SHOES.map((shoe) => (
+				<ShoeCard key={shoe.slug} {...shoe} />
+			))}
+		</Wrapper>
+	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	gap: 32px;
+`;
 
 export default ShoeGrid;
